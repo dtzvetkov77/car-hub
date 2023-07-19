5994f7592d950dee4176436d77b17bf2046a6a88
Daniel Tsvetkov <zvetkovd@gmail.com>
2023-07-20 00:39:23 +0300

    Implement car details modal functionalitty

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import { CustomFilter, Hero, SearchBar } from '@/components'
+import { CarCard, CustomFilter, Hero, SearchBar } from '@/components'
 import { fetchCars } from '@/utils'
 
 import Image from 'next/image'
 
+
+
 export default async function Home() {
   const allCars = await fetchCars();
+  console.log(allCars)
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars
   return (
     <main className="overflow-hidden">
@@ -27,7 +30,7 @@ export default async function Home() {
       <section>
         <div className='home__cars-wrapper'>
           {allCars?.map(car => (
-          <Carcard car={car}/>
+          <CarCard car={car}/>
           ))}
         </div>
       </section>
