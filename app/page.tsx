6186b56116ddb686940c64f0cb5562475eeee7ac
Daniel Tsvetkov <zvetkovd@gmail.com>
2023-08-07 15:26:42 +0300

    Fix Bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,100 +1,105 @@
-'use client'
-import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from '@/components'
-import { fuels, yearsOfProduction } from '@/constants';
-import { fetchCars } from '@/utils'
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
+"use client";
 
+import { useState, useEffect } from "react";
+import Image from "next/image";
 
+import { fetchCars } from "@/utils";
+import { fuels, yearsOfProduction } from "@/constants";
+import { CarCard, ShowMore, SearchBar, CustomFilter, Hero  } from "@/components";
+import { CarState } from "@/types";
 
 export default function Home() {
-  const [allCars, setAllCars] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [allCars, setAllCars] = useState<CarState>([]);
+  const [loading, setLoading] = useState(false);
 
-  const [manufacturer, setManufacturer] = useState('')
-  const [model, setModel] = useState('')
+  // search states
+  const [manufacturer, setManuFacturer] = useState("");
+  const [model, setModel] = useState("");
 
-  const [fuel, setFuel] = useState('');
-  const [year, setYear] = useState(2022)  
+  // filter state
+  const [fuel, setFuel] = useState("");
+  const [year, setYear] = useState(2022);
 
+  // limit state
   const [limit, setLimit] = useState(10);
 
   const getCars = async () => {
     setLoading(true);
     try {
       const result = await fetchCars({
-        manufacturer: manufacturer || '',
+        manufacturer: manufacturer.toLowerCase() || "",
+        model: model.toLowerCase() || "",
+        fuel: fuel.toLowerCase() || "",
         year: year || 2022,
-        fuel: fuel || '',
         limit: limit || 10,
-        model: model || '',
       });
-      setAllCars(result)
-    } catch (error) {
-      console.log(error);
+
+      setAllCars(result);
+    } catch {
+      console.error();
     } finally {
       setLoading(false);
     }
-  }
+  };
 
-  useEffect(()=> {
+  useEffect(() => {
     getCars();
-  }, [fuel, year, limit, manufacturer, model])
+  }, [fuel, year, limit, manufacturer, model]);
 
-
-  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars
   return (
-    <main className="overflow-hidden">
-     <Hero/>
-     <div className='mt-12 padding-x padding-y max-width'id="discover">
-      <div className='home__text-container'>
-        <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
-        <p>Explore the cars you might like</p>
-      </div>
+    <main className='overflow-hidden'>
+      <Hero />
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+
         <div className='home__filters'>
-            <SearchBar
-            setManufacturer={setManufacturer}
-            setModel={setModel}
-            />
+          <SearchBar setManuFacturer={setManuFacturer} setModel={setModel} />
+
           <div className='home__filter-container'>
-            <CustomFilter title='fuel' options={fuels} setFilter={setFuel} />
-            <CustomFilter title='year' options={yearsOfProduction} setFilter={setYear}/>
+            <CustomFilter options={fuels} setFilter={setFuel} />
+            <CustomFilter options={yearsOfProduction} setFilter={setYear} />
           </div>
         </div>
 
+        {allCars.length > 0 ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car, index) => (
+                <CarCard key={`car-${index}`} car={car} />
+              ))}
+            </div>
 
-    {allCars.length > 0 ? (
-      <section>
-        <div className='home__cars-wrapper'>
-          {allCars?.map(car => (
-          <CarCard car={car}/>
-          ))}
-        </div>
+            {loading && (
+              <div className='mt-16 w-full flex-center'>
+                <Image
+                  src='./loader.svg'
+                  alt='loader'
+                  width={50}
+                  height={50}
+                  className='object-contain'
+                />
+              </div>
+            )}
 
-        {loading && (
-          <div className='mt-16 w-full flex-center'>
-            <Image 
-            src='/loader.svg'
-            alt='loader'
-            width={50}
-            height={50}
-            className='object-contain'
+            <ShowMore
+              pageNumber={limit / 10}
+              isNext={limit > allCars.length}
+              setLimit={setLimit}
             />
-          </div>
+          </section>
+        ) : (
+          !loading && (
+            <div className='home__error-container'>
+              <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+              <p>{allCars?.message}</p>
+            </div>
+          )
         )}
-
-        <ShowMore
-        pageNumber={limit / 10}
-        isNext={limit > allCars.length}
-        setLimit={setLimit}
-        />
-      </section>
-    ): <div className='home__error-container'>
-      <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
-    </div>
-    }
-
-     </div>
+      </div>
     </main>
-  )
+  );
 }
--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,53 +1,68 @@
-'use client'
+"use client";
+
 import { Fragment, useState } from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import { Listbox, Transition } from "@headlessui/react";
-import { CustomFilterProps } from "@/types";
-import { updateSearchParams } from "@/utils";
 
-const CustomFilter = ({title, options, setFilter}: CustomFilterProps) => {
-  const [selected, setSelected] = useState(options[0])
+import { CustomFilterProps } from "@/types"; 
 
-
+export default function CustomFilter<T>({
+  options,
+  setFilter,
+}: CustomFilterProps<T>) {
+  const [menu, setMenu] = useState(options[0]); // State for storing the selected option
 
   return (
-    <div className="w-fit">
-      <Listbox 
-        value={selected}
+    <div className='w-fit'>
+      <Listbox
+        value={menu}
         onChange={(e) => {
-          setSelected(e)
-          setFilter(e.value)
-        }
-       }>
-        <div className="relative w-fit z-10" >
+          setMenu(e);
+          setFilter(e.value as unknown as T); // Update the selected option in state
+        }}
+      >
+        <div className='relative w-fit z-10'>
+          {/* Button for the listbox */}
           <Listbox.Button className='custom-filter__btn'>
-              <span className="block truncate">{selected.title}</span>
-              <Image 
-              src='chevron-up-down.svg'
+            <span className='block truncate'>{menu.title}</span>
+            <Image
+              src='/chevron-up-down.svg'
               width={20}
               height={20}
-              className="ml-4 object-contain"
-              alt='chevron-up-down'
-              />
+              className='ml-4 object-contain'
+              alt='chevron_up-down'
+            />
           </Listbox.Button>
-          <Transition 
-          as={Fragment}
-          leave="transition ease-in duration-100"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
+          {/* Transition for displaying the options */}
+          <Transition
+            as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
           >
             <Listbox.Options className='custom-filter__options'>
-              {options.map((option)=> (
+              {/* Map over the options and display them as listbox options */}
+              {options.map((option) => (
                 <Listbox.Option
-                key={option.title}
-                value={option}
-                className={({active}) => `relative cursor-default select-none py-2 px-4 ${active ? 'bg-primary-blue text-white' : 'text-gray-900' }`}
+                  key={option.title}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 px-4 ${
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
+                    }`
+                  }
+                  value={option}
                 >
-                  {({selected}) => (
-                      <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}` }>
+                  {({ selected }) => (
+                    <>
+                      {/* Display the option title */}
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
                         {option.title}
                       </span>
+                    </>
                   )}
                 </Listbox.Option>
               ))}
@@ -56,7 +71,5 @@ const CustomFilter = ({title, options, setFilter}: CustomFilterProps) => {
         </div>
       </Listbox>
     </div>
-  )
+  );
 }
-
-export default CustomFilter
